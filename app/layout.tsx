181a0d64d3605f95997d0e0a7bf57ac15e8fd1cf
uhnajs <117354@student.san.edu.pl>
2023-12-12 18:09:11 +0100

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
@@ -17,17 +14,13 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+
       */}
       <head />
 
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
         <Providers>
-          <Header />
           {children}
-          <Footer />
-          <ScrollToTop />
         </Providers>
       </body>
     </html>
--- app/pages/error.tsx
+++ app/pages/error.tsx
@@ -149,7 +149,7 @@ const ErrorPage = () => {
                   deleted or does not exist.
                 </p>
                 <Link
-                  href="/"
+                  href="/public"
                   className="px-8 py-3 text-base font-bold text-white duration-300 rounded-md bg-primary shadow-signUp hover:bg-white hover:text-primary md:px-9 lg:px-8 xl:px-9"
                 >
                   Back to Homepage
